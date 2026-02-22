2fe3d6040fe32eebac19160862d9d69cbded6ce7
YuanyuanZ07 <W0504770@nscc.ca>
2026-02-21 23:36:21 -0400

    Initial Halifax RoadWatch project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from "./pages/HomePage";
+import ReportPage from "./pages/ReportPage";
+import MapPage from "./pages/MapPage";
+import AdminPage from "./pages/AdminPage";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Halifax RoadWatch</h1>
+      <HomePage />
+      <ReportPage />
+      <MapPage />
+      <AdminPage />
     </div>
   );
 }
-
-export default App;
---
+++ src/pages/ReportPage.js
@@ -0,0 +1,3 @@
+export default function ReportPage() {
+  return <h2>Report</h2>;
+}
